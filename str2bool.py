b9db011611b4a790472e58601a86a42057ceb22a
kmonsoor <kmonsoor@users.noreply.github.com>
2015-01-08 03:31:59 +0600

    ++ support for some more languages

--- str2bool.py
+++ str2bool.py
@@ -1,7 +1,7 @@
 ## -*- coding: utf-8 -*-
 
 __title__ = 'String to Boolean'
-__version__ = '0.1'
+__version__ = '0.2'
 __author__ = 'khaled monsoor'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2015-2015 Hynek Schlawack'
@@ -9,8 +9,8 @@ __date_of_birth__ = '2015.01.07'
 
 
 # list of supported languages in ISO_639-1_code
-supported_languages = ['EN', 'BN', 'AR', 'NL']  
+supported_languages = ['EN', 'BN', 'AR', 'NL', 'ES', 'KO', 'JA', 'ZH', 'TL', 'VI',]  
 
 
 def string_to_boolean(string):
-    return str(string).lower() in ['yes', 'yeah', 'y', '1', 'ja', 'tak', 'yep', 'true', 'right', ]
+    return str(string).lower() in ['yes', 'yeah', 'y', '1', 'ja', 'tak', 'yep', 'true', 'right', 'si', u'sí', 'da', 'jes', u'হ্যা', u'জি', u'Да', u'हाँ', 'ya', u'já', 'hai', u'éé', 'eh', 'cha', 'baht', u'نعم', 'aye', 'bai', 'হাঁ', 'affirmative', 'positive', '+ve', '+', u'是的', u'oo', u'はい', u'예', u'네']
