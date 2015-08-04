f8a4ca214baf1b11ecb650ba41464ec97de14de1
kmonsoor <k@kmonsoor.com>
2015-08-04 18:14:48 +0600

    added exception, updated docstring

--- str2bool.py
+++ str2bool.py
@@ -1,19 +1,67 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-__title__ = 'String to Boolean'
-__version__ = '0.2'
-__author__ = 'khaled monsoor'
-__license__ = 'MIT'
-__copyright__ = 'Copyright 2015-2015 khaled monsoor'
-__date_of_birth__ = '2015.01.07'
+"""
+``string_to_boolean`` is a simple utility multi-lingual utility function to convert a string to boolean(True, False),
+by guessing the approximate meaning of the input string.
+By default, it will return `False` unless input is certainly a 'true'-ish word e.g. 'Positive', 'yeah' etc.
 
+Maybe, someday it will be able to detect "boolean"-lity of large sentences.
+But, now it can sense only basic words in many languages.
+
+Current supported languages:
+----------------------------
+ * English
+ * Bengali
+ * Dutch
+ * Polish
+ * Arabic
+ * Chinese
+ * Korean
+ * Spanish
+   And, many more ...
+
+Example usage
+-------------
+>>> import str2bool as s2b
+>>> print(s2b.supported_languages)
+['EN', 'BN', 'AR', 'NL', 'ES', 'KO', 'JA', 'ZH', 'TL', 'VI']
+>>> s2b.string_to_boolean('True')
+True
+>>> s2b.string_to_boolean('0')
+False
+>>> s2b.string_to_boolean('Tak')  # polish
+True
+>>> s2b.string_to_boolean('না')   # bengali "no"
+False
+>>> s2b.string_to_boolean('joe is a good boy')
+Traceback (most recent call last):
+  File "C:\Program Files (x86)\JetBrains\PyCharm 4.0.5\helpers\pycharm\docrunner.py", line 135, in __run
+    compileflags, 1), test.globs)
+  File "<doctest str2bool[6]>", line 1, in <module>
+    s2b.string_to_boolean('joe is a good boy')
+  File "C:/Users/k/PycharmProjects/str2bool/str2bool.py", line 51, in string_to_boolean
+    raise ValueError('Only single words are supported in this alpha-version utility function.')
+ValueError: Only single words are supported in this alpha-version utility function.
+"""
+
+# __version__ = '0.2'
+# __author__ = 'khaled monsoor <k@kmonsoor.com>'
+# __license__ = 'MIT'
+# __date_created__ = 2015.04.02
 
 # list of supported languages in ISO_639-1_code
 supported_languages = ['EN', 'BN', 'AR', 'NL', 'ES', 'KO', 'JA', 'ZH', 'TL', 'VI', ]
 
 
 def string_to_boolean(string):
-    return string.lower() in ['yes', 'yeah', 'y', '1', 'ja', 'tak', 'yep', 'true', 'right', 'si', 'sí', 'da',
-                              'jes', 'হ্যা', 'জি', 'Да', 'हाँ', 'ya', 'já', 'hai', 'éé', 'eh', 'cha', 'baht',
-                              'نعم', 'aye', 'bai', 'হাঁ', 'affirmative', 'positive', '+ve', '+', '是的', 'oo',
-                              'はい', '예', '네']
+    if len(string.split()) > 1:
+        raise ValueError('Only single words are supported in this alpha-version utility function.')
+    return string.lower() in ['yes', u'yeah', u'y', u'1', u'ja', u'tak', u'yep', u'true', u'right', u'si', u'sí', u'da',
+                              'jes', u'হ্যা', u'জি', u'Да', u'हाँ', u'ya', u'já', u'hai', u'éé', u'eh', u'cha', u'baht',
+                              'نعم', u'aye', u'bai', u'হাঁ', u'affirmative', u'positive', u'+ve', u'+', u'是的', u'oo',
+                              'はい', u'예', u'네']
+
+
+if __name__ == '__main__':
+    pass
