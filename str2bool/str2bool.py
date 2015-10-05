6424b02b569076aa850d916fdf905d554a46788a
Ivan Klishch <ivan@klishch.com>
2015-10-05 13:54:57 -0400

    Added support for Ukrainian language

--- str2bool/str2bool.py
+++ str2bool/str2bool.py
@@ -19,13 +19,14 @@ Current supported languages:
  * Chinese
  * Korean
  * Spanish
+ * Ukrainian
    And, many more ...
 
 Example usage
 -------------
 >>> import str2bool as s2b
 >>> print(s2b.supported_languages)
-['EN', 'BN', 'AR', 'NL', 'ES', 'KO', 'JA', 'ZH', 'TL', 'VI']
+['EN', 'BN', 'AR', 'NL', 'ES', 'KO', 'JA', 'ZH', 'TL', 'VI', 'UA']
 >>> s2b.string_to_boolean('True')
 True
 >>> s2b.string_to_boolean('0')
@@ -51,13 +52,13 @@ ValueError: Only single words are supported in this alpha-version utility functi
 # __date_created__ = 2015.04.02
 
 # list of supported languages in ISO_639-1_code
-supported_languages = ['EN', 'BN', 'AR', 'NL', 'ES', 'KO', 'JA', 'ZH', 'TL', 'VI', ]
+supported_languages = ['EN', 'BN', 'AR', 'NL', 'ES', 'KO', 'JA', 'ZH', 'TL', 'VI', 'UA']
 
 # list of checklist words that are considered "True"-ish
 truish_words = ['yes', u'yeah', u'y', u'1', u'ja', u'tak', u'yep', u'true', u'right', u'si', u'sí', u'da',
                 'jes', u'হ্যা', u'জি', u'Да', u'हाँ', u'ya', u'já', u'hai', u'éé', u'eh', u'cha', u'baht',
                 'نعم', u'aye', u'bai', u'হাঁ', u'affirmative', u'positive', u'+ve', u'+', u'是的', u'oo',
-                'はい', u'예', u'네']
+                'はい', u'예', u'네', u'так']
 
 
 def string_to_boolean(string):
